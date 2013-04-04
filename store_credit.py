26485599e1c59ba4cf038a2552c3d2e5aeeb8626
Nick <nhnatiw@gmail.com>
2013-04-04 13:58:16 -0300

    Update store_credit.py
    formatting

--- store_credit.py
+++ store_credit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # Problem
 #
-# You receive a credit C at a local store and would like to buy two items. You first walk through the store and create a list L of all available items. From this list you would like to buy two items that add up to the entire value of the credit. The solution you provide will consist of the two integers indicating the positions of the items in your list (smaller number first).
+# You receive a credit C at a local store and would like to buy two items. You first walk through the store and 
+# create a list L of all available items. From this list you would like to buy two items that add up to the entire
+# value of the credit. The solution you provide will consist of the two integers indicating the positions of the 
+# items in your list (smaller number first).
 #
 # Input
 #
@@ -9,12 +12,14 @@
 #
 #    One line containing the value C, the amount of credit you have at the store.
 #    One line containing the value I, the number of items in the store.
-#    One line containing a space separated list of I integers. Each integer P indicates the price of an item in the store.
+#    One line containing a space separated list of I integers. Each integer P indicates the price of an item in the 
+#      store.
 #    Each test case will have exactly one solution.
 #
 # Output
 #
-# For each test case, output one line containing "Case #x: " followed by the indices of the two items whose price adds up to the store credit. The lower index should be output first.
+# For each test case, output one line containing "Case #x: " followed by the indices of the two items whose price 
+#  adds up to the store credit. The lower index should be output first.
 #
 # Limits
 #
